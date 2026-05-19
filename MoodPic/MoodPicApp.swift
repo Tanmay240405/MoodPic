642910255f3329b290203adc30621b922c1e8ff8
Tanmay240405 <tanmaysaxena2404@gmail.com>
2026-05-19 15:36:31 +0530

    Initial Commit

---
+++ MoodPic/MoodPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodPicApp.swift
+//  MoodPic
+//
+//  Created by SDC-User on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
